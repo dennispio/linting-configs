dd62342b8d96b8ad2e5bfc68b1ea4314d0c65e77
Dennis Piotrowski <dennis.piotrowski@web.de>
2022-06-19 15:37:10 +0200

    first commit

---
+++ packages/eslint-react/src/index.js
@@ -0,0 +1,38 @@
+module.exports = {
+  root: true,
+  extends: ['plugin:react/recommended'],
+  plugins: ['react', 'react-hooks'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
+    },
+    'react': {
+      pragma: 'React',
+      version: 'detect'
+    }
+  },
+  rules: {
+    'react/jsx-curly-brace-presence': ['error', { props: 'never', children: 'never' }],
+    'react/react-in-jsx-scope': 'off',
+    'react-hooks/exhaustive-deps': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react/prop-types': 0,
+    'react/jsx-props-no-spreading': 'off',
+    'react/boolean-prop-naming': [
+      2,
+      {
+        rule: '^(is|has)[A-Z]([A-Za-z0-9]?)+',
+        message: 'Functions that return a boolean need to start with is or has.'
+      }
+    ],
+    'react/self-closing-comp': [
+      'error',
+      {
+        component: true,
+        html: true
+      }
+    ]
+  }
+}
---
+++ packages/prettier/src/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  printWidth: 100,
+  quoteProps: 'consistent',
+  semi: false,
+  tabWidth: 2,
+  singleQuote: true,
+  useTabs: false,
+  trailingComma: 'none'
+}
